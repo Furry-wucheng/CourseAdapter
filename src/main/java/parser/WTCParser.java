45b2a2398a4f5382e543525a76c35e1cb391eb08
Furry-wucheng <2538000780lig@gmail.com>
2023-02-15 15:26:23 +0800

    武汉职业技术学院适配

---
+++ src/main/java/parser/WTCParser.java
@@ -0,0 +1,9 @@
+package main.java.parser;/***
+ * @title WTCParser
+ * @description 
+ * @author WuCheng
+ * @version 1.0.0
+ * @create 2023/2/15 14:39
+**/
+public class WTCParser {
+}
---
+++ src/main/java/test/WTCTest.java
@@ -0,0 +1,9 @@
+package main.java.test;/***
+ * @title WTCTest
+ * @description 
+ * @author WuCheng
+ * @version 1.0.0
+ * @create 2023/2/15 14:37
+**/
+public class WTCTest {
+}
